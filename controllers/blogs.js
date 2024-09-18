e0cafca72a6419c94c3e015cd3c5a57b073f6f54
Kizuna Akari <lecion20@outlook.com>
2024-09-18 20:53:48 +0800

    13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const Blog = require("../models/blog");
+
+const blogRouter = express.Router();
+
+blogRouter.get('/', async (req, res) => {
+  const blogs = await Blog.findAll();
+  // console.log(JSON.stringify(blogs, null, 2))
+  return res.json(blogs)
+})
+blogRouter.post('/', async (req, res) => {
+  // console.log(req.body)
+
+  try {
+    const newblog = await Blog.create(req.body);
+    return res.json(newblog);
+  }
+  catch (error) {
+    return res.status(400).json({ error });
+  }
+})
+blogRouter.delete('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog)
+    await blog.destroy();
+  return res.status(204).end();
+})
+
+module.exports = blogRouter;
