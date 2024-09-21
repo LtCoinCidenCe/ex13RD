6e7fcdc842aef46e5b59670b932fc8be6da1975d
Kizuna Akari <lecion20@outlook.com>
2024-09-22 01:42:06 +0800

    13.19

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const Blog = require("./blog");
+const ReadingList = require("./readinglist");
 const User = require("./user");
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
 
 // const doSync = async () => {
 //   await User.sync({ alter: true })
---
+++ models/readinglist.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize")
+const { sequelize } = require("../util/db")
+
+class ReadingList extends Model { }
+ReadingList.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'users', key: 'id' },
+  },
+  blogId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: { model: 'blogs', key: 'id' },
+  },
+  state: {
+    type: DataTypes.CHAR(20),
+    allowNull: false,
+    defaultValue: 'unread',
+  }
+}, {
+  sequelize,
+  underscored: 'true',
+  modelName: 'readingList',
+})
+
+module.exports = ReadingList
